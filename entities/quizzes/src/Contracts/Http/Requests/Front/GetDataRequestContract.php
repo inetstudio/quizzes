d662708189959cd39932442f58bb3be330f95ea4
Konstantin Bukin <bukin@inetstudio.ru>
2020-03-24 19:31:32 +0300

    Update to 7.0

--- entities/quizzes/src/Contracts/Http/Requests/Front/GetDataRequestContract.php
+++ entities/quizzes/src/Contracts/Http/Requests/Front/GetDataRequestContract.php
@@ -1,12 +1,9 @@
 <?php
 
 namespace InetStudio\QuizzesPackage\Quizzes\Contracts\Http\Requests\Front;
-
-use InetStudio\AdminPanel\Base\Contracts\Http\Requests\BaseRequestContract;
-
 /**
  * Interface GetDataRequestContract.
  */
-interface GetDataRequestContract extends BaseRequestContract
+interface GetDataRequestContract
 {
 }
